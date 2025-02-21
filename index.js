cfee821bacc741d377de26394ca3aff4fd6bb5db
LionelAngerami <35354472+LionelAngerami@users.noreply.github.com>
2025-02-21 01:09:26 -0300

    ej3.11

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ app.use(express.json())
 const cors = require('cors')
 app.use(cors())
 
+app.use(express.static('dist'))
+
 morgan.token('post-data', (req, res) => req.method === 'POST' ? JSON.stringify(req.body) : '')
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post-data'))
 
